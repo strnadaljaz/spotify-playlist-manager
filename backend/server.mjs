c113adb0d1f5c6e449703d4513a35e29fc15baa5
strnadaljaz <strnadaljaz23@gmail.com>
2025-09-08 16:05:23 +0200

    getting playlists data to display them on dashboard

--- backend/server.mjs
+++ backend/server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { writeData, readData } from './database.mjs';
+import { getProfile, getPlaylistsData } from './spotify.mjs';
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,26 @@ const AuthLink = 'https://accounts.spotify.com/authorize?';
 const scope = 'playlist-read-private playlist-modify-public playlist-modify-private';
 const state = Math.random().toString(36).substring(2, 15);
 
+async function getNewTokens(refreshToken, clientId) {
+    const url = "https://accounts.spotify.com/api/token";
+
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_id: clientId
+      }),
+    }
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    return response;
+}
+
 function getAuthorizationUrl() {
     const url = AuthLink + new URLSearchParams({
         response_type: 'code',
@@ -30,20 +51,6 @@ function getAuthorizationUrl() {
     });
 
     return url;
-}
-
-async function getProfile(access_token) {
-    let accessToken = access_token;
-
-    const response = await fetch('https://api.spotify.com/v1/me', {
-        headers: {
-        Authorization: 'Bearer ' + accessToken
-        }
-    });
-
-    const data = await response.json();
-
-    return data;
 }
 
 app.post('/getAuthorizationUrl', (req, res) => {
@@ -89,25 +96,40 @@ app.post('/getAccessToken', async (req, res) => {
     }
 });
 
-async function getNewTokens(refreshToken, clientId) {
-    const url = "https://accounts.spotify.com/api/token";
+app.post('/getPlaylistsData', async (req, res) => {
+    const { spotify_id } = req.body;
 
-    const payload = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken,
-        client_id: clientId
-      }),
+    if (!spotify_id) {
+        return res.status(400).json({ error: 'spotify_id is required' });
     }
-    const body = await fetch(url, payload);
-    const response = await body.json();
+    try {
+        const data = readData(spotify_id);
 
-    return response;
-}
+        const access_token = data.access_token;
+        const refresh_token = data.refresh_token;
+        const expires = new Date(data.expires);
+
+        if (!(Date.now() + 120000 < expires)) {
+            const newData = await getNewTokens(refresh_token, clientID);
+            const new_access_token = newData.access_token;
+            const new_expires_in = newData.expires_in;
+            const now = Date.now();
+            const new_expires = new Date(now + new_expires_in * 1000);
+            const new_refresh_token = newData.refresh_token;
+
+            await writeData(spotify_id, new_access_token, new_refresh_token, new_expires.toString());
+
+            access_token = new_access_token;
+        }
+
+        const playlists_data = getPlaylistsData(spotify_id, access_token);
+
+        return playlists_data;
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // Handle token exchange - this is what your frontend calls
 app.post('/callback', async (req, res) => {
---
+++ backend/spotify.mjs
@@ -0,0 +1,31 @@
+async function getProfile(access_token) {
+    let accessToken = access_token;
+
+    const response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+        Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+}
+
+async function getPlaylistsData(user_id, access_token) {
+    const response = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${access_token}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        console.error(response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data;
+}
